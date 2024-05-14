2466009d2954e7463e13860de361feaa0c54bac9
cefuente <cfuen55010@gmail.com>
2024-05-14 14:39:19 +0200

    .

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cefuente <cefuente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:45:17 by cesar             #+#    #+#             */
-/*   Updated: 2024/05/14 11:45:18 by cesar            ###   ########.fr       */
+/*   Updated: 2024/05/14 13:23:02 by cefuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int	main (int argc, char **argv) {
+	if (!argv[0])
+		return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; argv[i]; i++) {
+		char *str = argv[i];
+		for (int j = 0; str[j]; j++) {
+			str[j] = toupper(str[j]);
+			std::cout << str[j];
+		}
+	}
+	return (0);
+}
