c5df557351e9b756dec52f3bef68274278537023
cesar <cesar.fuentes@outlook.fr>
2024-07-16 01:04:18 +0200

    CPP_07/ex00 ok

---
+++ CPP_07/ex00/includes/Template.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+template <typename T> void swap(T &a, T &b) {
+  T tmp;
+  tmp = a;
+  a = b;
+  b = tmp;
+}
+
+template <typename T> T &max(T &a, T &b) { return a > b ? a : b; }
+
+template <typename T> T &min(T &a, T &b) { return a < b ? a : b; }
