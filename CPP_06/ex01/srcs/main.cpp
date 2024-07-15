5d0a862247fbfe48c35540d8fff3505d463f5e6e
cesar <cesar.fuentes@outlook.fr>
2024-07-15 22:59:46 +0200

    added pointer comparison for CPP_06/ex01

--- CPP_06/ex01/srcs/main.cpp
+++ CPP_06/ex01/srcs/main.cpp
@@ -2,24 +2,27 @@
 #include <iostream>
 
 int main(void) {
-  Data person;
+  Data originalPerson;
   uintptr_t rawPerson;
   Data *rebornPerson;
 
-  person.age = 34;
-  person.name = "FlibbyFlop";
+  originalPerson.age = 34;
+  originalPerson.name = "FlibbyFlop";
 
   std::cout << "\tPre-serialized person : \n"
-            << person.name << ", " << person.age << "\n"
+            << originalPerson.name << ", age " << originalPerson.age << "\n"
             << std::endl;
 
-  rawPerson = Serializer::serialize(&person);
+  rawPerson = Serializer::serialize(&originalPerson);
   std::cout << "\tSerialized person : \n" << rawPerson << "\n" << std::endl;
 
   rebornPerson = Serializer::deserialize(rawPerson);
   std::cout << "\tDeserialized person : \n"
-            << rebornPerson->name << ", " << rebornPerson->age << "\n"
+            << rebornPerson->name << ", age " << rebornPerson->age << "\n"
             << std::endl;
 
+  std::cout << "\n\tPointer comparison :\n"
+            << "originalPerson pointer : " << &originalPerson
+            << "\nrebornPerson pointer : " << rebornPerson << std::endl;
   return 0;
 }
