16bae4b71463876f97903a28c157f1a84c8da091
cesar <cesar.fuentes@outlook.fr>
2024-07-15 08:23:04 +0200

    .

--- CPP_05/ex03/srcs/Intern.cpp
+++ CPP_05/ex03/srcs/Intern.cpp
@@ -1,6 +1,26 @@
 #include "Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequest.hpp"
+#include "ShrubberyCreationForm.hpp"
 
-AForm *Intern::makeForm(std::string type, std::string target) { return form; }
+AForm *makePresidentialPardonForm(std::string target) {
+  return new PresidentialPardonForm(target);
+}
+
+AForm *makeShrubberyCreationForm(std::string target) {
+  return new ShrubberyCreationForm(target);
+}
+
+AForm *makeRobotomyRequestForm(std::string target) {
+  return new RobotomyRequest(target);
+}
+
+AForm *Intern::makeForm(std::string type, std::string target) {
+  AForm *(*internChoice[])(std::string) = {makePresidentialPardonForm,
+                                           makeShrubberyCreationForm,
+                                           makeRobotomyRequestForm};
+}
+
 Intern::Intern() {}
 Intern::~Intern() {}
 Intern &Intern::operator=(const Intern &) {}
