b004edd8ad052d32a414cc869ea2d80e6e7ad910
cesar <cesar.fuentes@outlook.fr>
2024-07-16 13:37:06 +0200

    CPP_07/ex02 ok

---
+++ CPP_07/ex02/includes/Array.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstddef>
+#include <exception>
+
+template <typename T> class Array {
+private:
+  T *_arr;
+  unsigned int _n;
+
+public:
+  Array() : _arr(NULL), _n(0) {}
+
+  ~Array() { delete[] _arr; }
+
+  Array(unsigned int n) : _arr(new T[n]), _n(n) {}
+
+  Array &operator=(const Array &src) {
+    if (this == &src)
+      return *this;
+    if (this->_n != src._n) {
+      if (this->_arr)
+        delete[] this->_arr;
+      this->_arr = new T[src._n];
+      this->_n = src._n;
+    }
+    for (int i = 0; i < _n; ++i) {
+      this->_arr[i] = src._arr[i];
+    }
+    return *this;
+  }
+
+  T &operator[](unsigned int index) {
+    if (index >= _n)
+      throw(Array::InvalidIndexException());
+    return _arr[index];
+  }
+
+  std::size_t size() { return _n; }
+
+  class InvalidIndexException : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "Invalid index"; }
+  };
+};
---
+++ CPP_07/ex02/srcs/main.cpp
@@ -0,0 +1,34 @@
+#include "Array.hpp"
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+
+  Array<int> intArr(3);
+  intArr[0] = 234;
+  intArr[1] = 3;
+  intArr[2] = 2200;
+
+  Array<std::string> stringArr(3);
+  stringArr[0] = "BOUH";
+  stringArr[1] = "BOUUH";
+  stringArr[2] = "BOUUH";
+
+  for (std::size_t i = 0; i < intArr.size(); ++i) {
+    std::cout << intArr[i] << " ";
+  }
+  std::cout << std::endl;
+
+  for (std::size_t i = 0; i < stringArr.size(); ++i) {
+    std::cout << stringArr[i] << " ";
+  }
+  std::cout << std::endl;
+
+  try {
+    std::cout << stringArr[3] << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
