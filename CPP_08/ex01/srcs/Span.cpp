2c1123a6065613368124ffba40822b0ebe22bac0
cefuente <cfuen55010@gmail.com>
2024-07-17 09:47:45 +0200

    .

--- CPP_08/ex01/srcs/Span.cpp
+++ CPP_08/ex01/srcs/Span.cpp
@@ -1,8 +1,10 @@
 #include "Span.hpp"
 
+void Span::addNumber(int n) { if () }
+
 Span::Span() {}
 Span::~Span() {}
-Span &Span::operator=(const Span &) { return *this; }
+Span &Span::operator=() { return *this; }
 Span::Span(const Span &) {}
 
 const char *Span::outOfRangeException::what() const throw() {
